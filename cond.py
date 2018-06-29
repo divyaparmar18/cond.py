56599dc53acf83682d5a396e54dbdd2f5111b048
Annu <you@example.com>
2018-06-29 11:44:00 +0530

    first file

---
+++ cond.py
@@ -0,0 +1,69 @@
+#condition
+
+a = 44
+
+b = 56
+
+if a  < b:
+
+    print ("a is smaller than b")
+
+a = 100
+
+b = 67
+
+if a > b:
+
+  print ("a is greater than b")
+
+elif a == a:
+
+  print ("a is equal to b")
+a = 200
+
+b = 98
+
+if b > a:
+  print ("b is greater than a")
+
+elif b == a:
+  print ("b is equal to a")
+
+else:
+  print ("a is greater than b")
+
+# print i as long as i is smallerthan 6
+
+i = 1
+
+while i < 6:
+
+  print(i)
+
+  i += 1
+
+i = 1
+
+while i < 6:
+
+  print (i)
+
+  if (i == 3):
+
+    break
+
+  i += 1
+
+i = 0
+
+while i < 6:
+
+  i += 1
+
+  if i == 3:
+
+     continue
+
+  print (i)
+
+
